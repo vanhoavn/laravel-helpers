b6668a0af6485a0caca3e6ec9ab734a1b0f8b9c9
Vinh <npvinh91@gmail.com>
2019-05-12 01:09:49 +0700

    fix test command

--- Console/ModuleMakeCommands/ModuleTestMakeCommand.php
+++ Console/ModuleMakeCommands/ModuleTestMakeCommand.php
@@ -4,22 +4,34 @@ namespace Vhnvn\LaravelHelper\Console\ModuleMakeCommands;
 
 class ModuleTestMakeCommand extends \Illuminate\Foundation\Console\TestMakeCommand
 {
-  use ModuleContextOverride;
-  /**
-	 * The console command signature.
-   *
-   * @var string
-   */
-  protected $signature = 'mmake:test
+    use ModuleContextOverride;
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'mmake:test
     {module           : The module to create the listener.}
     {name             : The name of the class.}
     {--unit : Create a unit test}
   ';
 
-  /**
-   * The console command description.
-   *
-   * @var string
-   */
-  protected $description = 'Create a new module\'s listener class';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new module\'s test class';
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\\Test';
+    }
 }
