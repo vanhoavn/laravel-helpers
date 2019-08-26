ca167be3185b0e71787a7309ee40a4f975b82e42
Hoa Nguyen Van <vanhoa@vhn.vn>
2019-08-26 17:24:27 +0700

    update for 5.8

--- Console/ModuleMakeCommands/ModuleMigrateMakeCommand.php
+++ Console/ModuleMakeCommands/ModuleMigrateMakeCommand.php
@@ -14,9 +14,11 @@ class ModuleMigrateMakeCommand extends \Illuminate\Database\Console\Migrations\M
     protected $signature = 'mmake:migration
         {module : The name of the module.}
         {name : The name of the migration.}
-        {--create= : The table to be created.}
-        {--table= : The table to migrate.}
-        {--path= : The location where the migration file should be created.}';
+        {--create= : The table to be created}
+        {--table= : The table to migrate}
+        {--path= : The location where the migration file should be created}
+        {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
+        {--fullpath : Output the full path of the migration}';
 
 
     /**
